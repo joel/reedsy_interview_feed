f205abd6c77acbe073d6ace985bae9c554017c7e
Joel AZEMAR <joel.azemar@gmail.com>
2018-10-04 16:00:26 +0200

    Add Feed Class

---
+++ examples/sample.rb
@@ -0,0 +1,29 @@
+#Create an users
+user = ReedsyInterviewFeed::Models::User.create({ name: 'Joel AZEMAR' })
+
+# Create Author
+author = ReedsyInterviewFeed::Models::Author.create({ name: 'Yuval Noha Harari' })
+author_craig_l_symonds = ReedsyInterviewFeed::Models::Author.create({ name: 'Craig L. Symonds' })
+
+# Add a bunch of books
+ReedsyInterviewFeed::Models::Book.create({ title: 'Sapiens', author: author })
+ReedsyInterviewFeed::Models::Book.create({ title: 'Homo Deus', author: author })
+ReedsyInterviewFeed::Models::Book.create({ title: '21 Lessons for the 21st Century', author: author })
+
+# Follow this author
+ReedsyInterviewFeed::Services::FollowAnAuthor.new(user: user, author: author).call
+
+# Add another book and star it
+book = ReedsyInterviewFeed::Models::Book.create({ title: 'Alan Turing: The Enigma' })
+ReedsyInterviewFeed::Services::Upvote.new(user: user, book: book).call
+
+ReedsyInterviewFeed::Models::Book.create({ title: 'Antifragile: Things That Gain from Disorder' })
+
+ReedsyInterviewFeed::Models::Book.create({ title: 'World War II at Sea: A Global History', author: author_craig_l_symonds })
+
+# Get the feed of the user
+feed = ReedsyInterviewFeed::Services::Feed.new(user)
+
+# Get the title of the following books
+feed.retrieve.map(&:title)
+#=> ["Sapiens", "Homo Deus", "21 Lessons for the 21st Century", "Alan Turing: The Enigma"]
--- lib/reedsy_interview_feed/models/book.rb
+++ lib/reedsy_interview_feed/models/book.rb
@@ -2,6 +2,7 @@ module ReedsyInterviewFeed
   module Models
     class Book < Base
       belongs_to :author
+      has_many :likes
       has_many :follows
     end
   end
